662c80c5e899cb66364003757abdf737de80deca
Yan Wei <yanwei@Yans-MacBook-Pro.local>
2021-12-15 13:57:11 -0800

    fix consumer

---
+++ Consumer/src/main/java/Consumer.java
@@ -0,0 +1,79 @@
+import com.google.gson.Gson;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class Consumer {
+
+    private static Gson gson;
+    private static Map<Integer, List<LiftRide>> map;
+
+    private static String HostName = "35.163.51.25";
+    private static int PortName = 5672;
+    private static String UserName = "admin";
+    private static String PassWord = "admin";
+    private static String QueueName = "post_queue";
+    private static int numThreads;
+
+    public static void main(String[] args)
+            throws IOException, TimeoutException, InterruptedException {
+        map = new ConcurrentHashMap<Integer, List<LiftRide>>();
+        gson = new Gson();
+        numThreads = Integer.parseInt(args[0]);
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HostName);
+        factory.setPort(PortName);
+        factory.setUsername(UserName);
+        factory.setPassword(PassWord);
+        Connection connection = factory.newConnection();
+        AtomicInteger count = new AtomicInteger(0);
+
+        Runnable runnable = new Runnable() {
+            Channel channel = null;
+            public void run() {
+                try {
+                    channel = connection.createChannel();
+                    channel.queueDeclare(QueueName, true, false, false, null);
+                    channel.basicQos(1);
+
+                    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                        String message = new String(delivery.getBody(), "UTF-8");
+//                        System.out.println("message is: " + message);
+                        channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                        LiftRide liftRide = gson.fromJson(message, LiftRide.class);
+                        int skierID = liftRide.getSkierID();
+                        if (!map.containsKey(skierID)) {
+                            map.put(skierID, new ArrayList<>());
+                        }
+                        map.get(skierID).add(liftRide);
+                    };
+
+                    channel.basicConsume(QueueName, false, deliverCallback, consumertag -> {
+                    });
+
+                    count.getAndIncrement();
+                    if (count.get() % 1000 == 0) {
+                        System.out.println("pulling " + count.get() + "th message");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        for (int i = 0; i < numThreads; i++) {
+            Thread thread = new Thread(runnable);
+            thread.start();
+        }
+
+    }
+}
---
+++ Consumer/src/main/java/LiftRide.java
@@ -0,0 +1,67 @@
+public class LiftRide {
+    private int time;
+    private int liftID;
+    private int skierID;
+    private int resortID;
+    private int seasonID;
+    private int dayID;
+
+
+    public LiftRide(int time, int liftID, int skierID, int resortID, int seasonID, int dayID) {
+        this.time = time;
+        this.liftID = liftID;
+        this.skierID = skierID;
+        this.resortID = resortID;
+        this.seasonID = seasonID;
+        this.dayID = dayID;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setLiftID(int liftID) {
+        this.liftID = liftID;
+    }
+
+    public void setSkierID(int skierID) {
+        this.skierID = skierID;
+    }
+
+    public void setResortID(int resortID) {
+        this.resortID = resortID;
+    }
+
+    public void setSeasonID(int seasonID) {
+        this.seasonID = seasonID;
+    }
+
+    public void setDayID(int dayID) {
+        this.dayID = dayID;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getLiftID() {
+        return liftID;
+    }
+
+    public int getSkierID() {
+        return skierID;
+    }
+
+    public int getResortID() {
+        return resortID;
+    }
+
+    public int getSeasonID() {
+        return seasonID;
+    }
+
+    public int getDayID() {
+        return dayID;
+    }
+}
+
