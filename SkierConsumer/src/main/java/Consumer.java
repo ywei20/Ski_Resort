a1b381b2f9bdf4ec5b442b7b69abb75a48787e9e
Yan Wei <yanwei@Yans-MacBook-Pro.local>
2021-12-18 23:24:42 -0800

    fix skier consumer

--- SkierConsumer/src/main/java/Consumer.java
+++ SkierConsumer/src/main/java/Consumer.java
@@ -33,36 +33,35 @@ public class Consumer {
         factory.setPassword(PassWord);
         Connection connection = factory.newConnection();
 
-        Runnable runnable = new Runnable() {
-            Channel channel = null;
-            public void run() {
-                try {
-                    channel = connection.createChannel();
-                    channel.exchangeDeclare(ExchangeName, BuiltinExchangeType.FANOUT);
-                    channel.queueDeclare(QueueName, true, false, false, null);
-                    channel.queueBind(QueueName, ExchangeName, "");
-                    channel.basicQos(100);
-
-                    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                        String message = new String(delivery.getBody(), "UTF-8");
-                        channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-
-                        LiftRide liftRide = gson.fromJson(message, LiftRide.class);
-                        liftRide.setVertical(100); // manually set vertical value to 100 based on Piazza post
-                        liftRideDao.createLiftRide(liftRide);
-
-                    };
-
-                    channel.basicConsume(QueueName, false, deliverCallback, consumertag -> {});
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
         for (int i = 0; i < numThreads; i++) {
-            Thread thread = new Thread(runnable);
+            Thread thread = new Thread(new Runnable() {
+                Channel channel = null;
+                @Override
+                public void run() {
+                    try {
+                        channel = connection.createChannel();
+                        channel.exchangeDeclare(ExchangeName, BuiltinExchangeType.FANOUT);
+                        channel.queueDeclare(QueueName, true, false, false, null);
+                        channel.queueBind(QueueName, ExchangeName, "");
+                        channel.basicQos(1);
+
+                        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                            String message = new String(delivery.getBody(), "UTF-8");
+                            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+
+                            LiftRide liftRide = gson.fromJson(message, LiftRide.class);
+                            liftRide.setVertical(100); // manually set vertical value to 100 based on Piazza post
+                            liftRideDao.createLiftRide(liftRide);
+
+                        };
+
+                        channel.basicConsume(QueueName, false, deliverCallback, consumertag -> {});
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
             thread.start();
         }
 
