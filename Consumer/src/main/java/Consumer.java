e5f5981c7f1b49eaa9f8af9a7a3da4da02bbbb95
Yan Wei <yanwei@Yans-MacBook-Pro.local>
2021-12-23 18:01:01 -0800

    fix consumer multithread method

--- Consumer/src/main/java/Consumer.java
+++ Consumer/src/main/java/Consumer.java
@@ -17,11 +17,11 @@ public class Consumer {
     private static Gson gson;
     private static Map<Integer, List<LiftRide>> map;
 
-    private static String HostName = "35.163.51.25";
+    private static String HostName = "3.83.23.114";
     private static int PortName = 5672;
     private static String UserName = "admin";
     private static String PassWord = "admin";
-    private static String QueueName = "post_queue";
+    private static String QueueName = "skiers_queue";
     private static int numThreads;
 
     public static void main(String[] args)
@@ -37,42 +37,39 @@ public class Consumer {
         Connection connection = factory.newConnection();
         AtomicInteger count = new AtomicInteger(0);
 
-        Runnable runnable = new Runnable() {
-            Channel channel = null;
-            public void run() {
-                try {
-                    channel = connection.createChannel();
-                    channel.queueDeclare(QueueName, true, false, false, null);
-                    channel.basicQos(1);
-
-                    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                        String message = new String(delivery.getBody(), "UTF-8");
-//                        System.out.println("message is: " + message);
-                        channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-                        LiftRide liftRide = gson.fromJson(message, LiftRide.class);
-                        int skierID = liftRide.getSkierID();
-                        if (!map.containsKey(skierID)) {
-                            map.put(skierID, new ArrayList<>());
-                        }
-                        map.get(skierID).add(liftRide);
-                    };
-
-                    channel.basicConsume(QueueName, false, deliverCallback, consumertag -> {
-                    });
-
-                    count.getAndIncrement();
-                    if (count.get() % 1000 == 0) {
-                        System.out.println("pulling " + count.get() + "th message");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
         for (int i = 0; i < numThreads; i++) {
-            Thread thread = new Thread(runnable);
-            thread.start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Channel channel = connection.createChannel();
+                        channel.queueDeclare(QueueName, true, false, false, null);
+                        channel.basicQos(1);
+
+                        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                            String message = new String(delivery.getBody(), "UTF-8");
+//                        System.out.println("message is: " + message);
+                            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                            LiftRide liftRide = gson.fromJson(message, LiftRide.class);
+                            int skierID = liftRide.getSkierID();
+                            if (!map.containsKey(skierID)) {
+                                map.put(skierID, new ArrayList<>());
+                            }
+                            map.get(skierID).add(liftRide);
+                        };
+
+                        channel.basicConsume(QueueName, false, deliverCallback, consumertag -> {
+                        });
+
+                        count.getAndIncrement();
+                        if (count.get() % 1000 == 0) {
+                            System.out.println("pulling " + count.get() + "th message");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
 
     }
